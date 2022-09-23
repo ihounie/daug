95dd55166ddd4e9b1fd2f80b726a2d8aadef83dd
ihounie <ihounie@gmail.com>
2022-09-23 09:37:16 -0400

    fix

--- TrivialAugment/train_pd_batch.py
+++ TrivialAugment/train_pd_batch.py
@@ -113,7 +113,7 @@ def run_epoch_dual(rank, worldsize, model, loader, loss_fn, optimizer, dual_vars
         for i in  range(1, mh_steps):
             iter_loader = iter(aug_loader)
             aug_data, _ , op, level = next(iter_loader)
-            aug_data = aug_datato(f"cuda:{rank}")
+            aug_data = aug_data.to(f"cuda:{rank}")
             proposals[i*batch_size:(i+1)*batch_size] = aug_data
             ops[i*batch_size:(i+1)*batch_size] = op
             levels[i*batch_size:(i+1)*batch_size] = level
