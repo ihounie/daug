247284ae177f2c8b8cd1006777c13f494d7936d4
ihounie <ihounie@seas.upenn.edu>
2022-09-24 15:17:11 -0400

    scripts

--- scripts/grid-search/RA/run_ablation_margin_cifar100.sh
+++ scripts/grid-search/RA/run_ablation_margin_cifar100.sh
@@ -4,6 +4,6 @@ for seed in 0 1 2 3
 do
 	for margin in 0.6 0.9 1.2 1.5 1.8
 	do
-		python -m TrivialAugment.train_pd_batch -c confs/grid-search/RA/pd_cifar100_ablation__seed=${seed}__PD.margin=${margin}__1try.yaml --dataroot data --tag PD_ablation_CIFAR10 --wandb_log --save results/PD_single_CIFAR100_${margin}_${seed} --local_rank ${LOCAL_RANK}
+		python -m TrivialAugment.train_pd_batch -c confs/grid-search/RA/pd_cifar100_ablation__seed=${seed}__PD.margin=${margin}__1try.yaml --dataroot data --tag PD_ablation_CIFAR10 --wandb_log --save results/PD_single_CIFAR100_${margin}_${seed} --local_rank ${LOCAL_RANK} --cv-ratio 0.1
 	done
 done
