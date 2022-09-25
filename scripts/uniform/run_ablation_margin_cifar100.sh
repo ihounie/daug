b12035b09ee2e3086e9b8d8b2e7ebddd7708e622
ihounie <ihounie@seas.upenn.edu>
2022-09-25 15:11:27 -0400

    fixi

--- scripts/uniform/run_ablation_margin_cifar100.sh
+++ scripts/uniform/run_ablation_margin_cifar100.sh
@@ -1,9 +1,9 @@
 LOCAL_RANK=0
-python create_variants_of_set_config.py confs/uniform/pd_cifar10_ablation.yaml 1
+python create_variants_of_set_config.py confs/uniform/pd_cifar100_ablation.yaml 1
 for seed in 0 1 2 3 4
 do
-	for margin in 0.6 0.9 1.2
+	for margin in 1.2 0.9 0.6
 	do
-		python -m TrivialAugment.train_pd_batch -c confs/uniform/pd_cifar10_ablation__seed=${seed}__PD.margin=${margin}__1try.yaml --dataroot data --tag PD_ablation_CIFAR10 --wandb_log --save results/PD_single_CIFAR100_${margin}_${seed} --local_rank ${LOCAL_RANK} --cv-ratio 0.1
+		python -m TrivialAugment.train_pd_batch -c confs/uniform/pd_cifar100_ablation__seed=${seed}__PD.margin=${margin}__1try.yaml --dataroot data --tag PD_ablation_CIFAR10 --wandb_log --save results/PD_single_CIFAR100_${margin}_${seed} --local_rank ${LOCAL_RANK} --cv-ratio 0.1
 	done
 done
