5c5061eec6ac39581a0300660ca1e8ae4f7a236e
ihounie <ihounie@seas.iupenn.edu>
2023-05-24 14:48:57 -0400

    scripts fix gpu

--- scripts/imnet/baselines/adam_10.sh
+++ scripts/imnet/baselines/adam_10.sh
@@ -3,5 +3,5 @@ method="baselines"
 python create_variants_of_set_config.py confs/imnet/${method}/adam_10.yaml 1
 for seed in 1 2 3 4
 do
-	CUDA_SET_VISIBLE_DEVICES=${local_rank} python -m TrivialAugment.train_pd_batch -c confs/imnet/${method}/adam_10__seed=${seed}__1try.yaml --dataroot ~/imnet-100-data/ --tag PD_Adam_10 --wandb_log --save results/imnet_pd_${seed} --project imnet100
+	CUDA_VISIBLE_DEVICES=${local_rank} python -m TrivialAugment.train -c confs/imnet/${method}/adam_10__seed=${seed}__1try.yaml --dataroot ~/imnet-100-data/ --tag PD_Adam_10 --wandb_log --save results/imnet_pd_${seed} --project imnet100  --local_rank 0
 done
